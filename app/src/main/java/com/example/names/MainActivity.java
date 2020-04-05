c58d66a6896b5fc1cda5ebef4508cd870e3fe4c7
AHMED ALBLWI <alblwi@gmail.com>
2020-04-05 03:49:28 +0300

    Adding many things bro :)
    First one of my student's . Project , and I just added some of code for him to understand the first idea.

--- app/src/main/java/com/example/names/MainActivity.java
+++ app/src/main/java/com/example/names/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.names;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button but1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        but1 = findViewById(R.id.button1);
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondAct.class);
+                startActivity(intent);
+            }
+        });
     }
 }
